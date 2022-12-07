b6af74ba851f8f31f958d0548782ad6edb88487c
bakuful <dubakufulco@gmail.com>
2022-12-07 05:28:57 -0500

    adjust task and evm decoder

--- evmv2/tasks/swapOutToken.js
+++ evmv2/tasks/swapOutToken.js
@@ -1,52 +1,58 @@
 
 function stringToHex(str) {
-    return str.split("").map(function(c) {
+    return str.split("").map(function (c) {
         return ("0" + c.charCodeAt(0).toString(16)).slice(-2);
     }).join("");
 }
 
-const bscSwapData = {
-    swapParams: [
-        {
-            amountIn: '1000000000000000000', // 1 USDC
-            minAmountOut: '0',
-            path: '0x64544969ed7EBf5f083679233325356EbE738930094616F0BdFB0b526bD735Bf66Eca0Ad254ca81F', // usdc - wbnb
-            routerIndex: 0 // pancake
-        }
-    ],
-    targetToken: '0x0000000000000000000000000000000000000000',
-    mapTargetToken: '0x0000000000000000000000000000000000000000' // receiver address
-}
-
-const maticSwapData = {
-    swapParams: [
-        {
-            amountIn: '1000000000000000000', // 1 USDC
-            minAmountOut: '0',
-            path: '0x64544969ed7EBf5f083679233325356EbE738930094616F0BdFB0b526bD735Bf66Eca0Ad254ca81F', // usdc - wmatic
-            routerIndex: 0 // quick
-        }
-    ],
-    targetToken: '0x0000000000000000000000000000000000000000',
-    mapTargetToken: '0x0000000000000000000000000000000000000000'
-}
-
-
 module.exports = async (taskArgs) => {
-    const accounts = await ethers.getSigners()
+    const abi = ethers.utils.defaultAbiCoder;
+    const bscSwapData = abi.encode(
+        ["tuple(uint256, uint256, bytes, uint64)[]", "bytes", "address"],
+
+        [
+            [[
+                "1000000000000000000", // 1 USDC
+                "0",
+                '0x64544969ed7EBf5f083679233325356EbE738930094616F0BdFB0b526bD735Bf66Eca0Ad254ca81F', // usdc - wbnb
+                "0" // pancake
+            ]]
+            ,
+            '0x0000000000000000000000000000000000001111',
+            '0x0000000000000000000000000000000000000000'
+        ]
+    );
+
+    const maticSwapData = abi.encode(
+        ["tuple(uint256, uint256, bytes, uint64)[]", "bytes", "address"],
+
+        [
+            [[
+                "1000000000000000000", // 1 USDC
+                "0",
+                '0x64544969ed7EBf5f083679233325356EbE738930094616F0BdFB0b526bD735Bf66Eca0Ad254ca81F', // usdc - wbnb
+                "0" // pancake
+            ]]
+            ,
+            '0x0000000000000000000000000000000000002222',
+            '0x0000000000000000000000000000000000000000'
+        ]
+    );
+
+    const accounts = await ethers.getSigners();
     const deployer = accounts[0];
 
-    console.log("deployer address:",deployer.address);
+    console.log("deployer address:", deployer.address);
 
     let token = await ethers.getContractAt("IERC20", taskArgs.token);
 
-    let mos = await ethers.getContractAt('MAPOmnichainServiceV2',taskArgs.mos);
+    let mos = await ethers.getContractAt('MAPOmnichainServiceV2', taskArgs.mos);
 
     let address = taskArgs.address;
     if (taskArgs.address === "") {
         address = deployer.address;
     } else {
-        if (taskArgs.address.substr(0,2) != "0x") {
+        if (taskArgs.address.substr(0, 2) != "0x") {
             address = "0x" + stringToHex(taskArgs.address);
         }
     }
@@ -57,11 +63,12 @@ module.exports = async (taskArgs) => {
         toChainSwapData = maticSwapData;
     }
 
-    if (taskArgs.token === "0x0000000000000000000000000000000000000000"){
+    if (taskArgs.token === "0x0000000000000000000000000000000000000000") {
         await (await mos.connect(deployer).swapOutNative(
             address,
             taskArgs.tochain,
-            {value:taskArgs.value}
+            toChainSwapData,
+            {value: taskArgs.value}
         )).wait();
     } else {
         await (await token.connect(deployer).approve(
@@ -71,8 +78,8 @@ module.exports = async (taskArgs) => {
 
         await (await mos.connect(deployer).swapOutToken(
             taskArgs.token,
+            address,
             taskArgs.value,
-            taskArgs.mapTargetToken,
             taskArgs.tochain,
             toChainSwapData
         )).wait();
